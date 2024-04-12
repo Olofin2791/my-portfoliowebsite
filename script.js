80da8986bca6f22650749676ff5f3c2432d21f6f
Olofin2791 <Olofinabiodun42@gmail.com>
2024-04-12 19:18:06 +0100

    my first commit

--- script.js
+++ script.js
@@ -1,24 +1,6 @@
-let contact = function sendEmail() {
-  Email.send({
-    Host: "smtp.gmail.com",
-    Username: "olofinabiodun42@gmail.com",
-    Password: "Olofin2791",
-    To: "olofinabiodun42@gmail.com",
-    From: document.getElementById("email").value,
-    Subject: "Contact form",
-    Body:
-      "Name: " +
-      document.getElementById("name").value +
-      "<br> Email:" +
-      document.getElementById("email").value +
-      "<br> write:" +
-      document.getElementById("Submit").value,
-  }).then((message) => alert("Message Sent"));
-};
+const morebutton = document.getElementById("more");
 
-let readMoreBtn = document.querySelector(".read-more-btn");
-let text = document.querySelector(".text");
-
-readMoreBtn.addEventListener("click", (e) => {
-  text.classList.toggle("show-more");
+morebutton.addEventListener("click", function (e) {
+  // console.log("thank you");
+  document.body.innerHTML = "<h3> THANK YOU </h3>";
 });
